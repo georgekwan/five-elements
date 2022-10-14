16544d2d0c3ff77cda200941cd92ff38bb998ceb
George <gwmkwan@gmail.com>
2022-10-13 18:52:12 -0600

    Bug fixes for decisions in the intros

--- projectOne.js
+++ projectOne.js
@@ -1,21 +1,19 @@
 import rl from "readline-sync";
 ("use strict");
 
-const yesNo = ["y", "n"];
 function gameStart(playGame) {
-  console.log("\n----------------\nThe commands are (y)es or (n)o:", yesNo);
-  playGame = rl.question("are you ready for an adventure? (y/n) ");
+  console.log("\n----------------\nThe commands are (y)es or (n)o");
+  playGame = rl.question(
+    "\nAre you ready for to go on an adventure as Chubby? (y/n) "
+  );
   while (playGame != "y") {
     if (playGame === "y") {
       break;
     } else if (playGame === "n") {
-      console.log(
-        "\n----------------\nPlease enter (y)es when you are ready.:",
-        yesNo
-      );
-      playGame = rl.question("are you ready for an adventure? (y/n) ");
+      console.log("\n----------------\nPlease enter (y)es when you are ready.");
+      playGame = rl.question("Are you ready for an adventure? (y/n) ");
     } else {
-      console.log("Come on!!! Is it that hard to enter (y)es or (n)o?", yesNo);
+      console.log("Come on!!! Is it that hard to enter (y)es or (n)o?");
       playGame = rl.question("Now... are you ready for an adventure?! (y/n) ");
     }
   }
@@ -26,77 +24,92 @@ function gameStart(playGame) {
 
 function lookMom() {
   console.log(
-    "\nYou awoke at home, but your mother was not to be seen, and you are concerned for her since you are hungry."
-  );
-  console.log(
-    "\nSo you decided to walk outdoors and look around for your mom."
-  );
-  console.log(
-    "\nBecause your mom has gone missing, you've chosen to walk into the wilderness and look for her."
+    "\nYou awoke at home, but your mother was not to be seen, and you are concerned for her since you are hungry.\n\nSo you decided to walk outdoors and look around for your mom.\n\nBecause your mom has gone missing, you've chosen to walk into the wilderness and look for her."
   );
 }
-
+let crimsonDoor;
 function enterForest(eatMush) {
   console.log(
     "\nYou spotted a nice-looking mushroom not long after entering the wilderness, and you're starving."
   );
-  console.log("\n----------------\nThe commands are (y)es or (n)o:", yesNo);
-  eatMush = rl.question("Are you going to eat it? (y/n) ");
+  console.log("\n----------------\nThe commands are (y)es or (n)o");
+  eatMush = rl.question("\nAre you going to eat it? (y/n) ");
   console.log("\n");
   while (eatMush != "y") {
     if (eatMush === "y") {
-      console.log(
-        "\nYou weren't feeling well, so you opted to relax behind a massive tree."
-      );
-      console.log(
-        "\nYou see something on the other side of the tree as you get closer to it."
-      );
+      break;
+    } else if (eatMush === "n") {
+      console.log("\nTaking a look around");
+      crimsonDoor = "n";
       break;
     } else {
-      console.log("\nTaking a look around");
-      // this part needs to connect to "You choose to ignore it by taking a nap."
-      massiveTree;
+      console.log("Hey little buddy, make up your mind!!! (y)es or (n)o?");
+      eatMush = rl.question("Are you eating that beautiful mushroom?! (y/n) ");
     }
   }
+  console.log(
+    "\nYou weren't feeling well, so you opted to relax behind a massive tree.\n\nYou see something on the other side of the tree as you get closer to it."
+  );
 }
 
 function massiveTree(crimsonDoor) {
   console.log("\nSo you went to have a peek and discovered a crimson door.");
-  console.log("\n----------------\nThe commands are (y)es or (n)o:", yesNo);
-  crimsonDoor = rl.question("Do you want to knock? (y/n) ");
-  if (crimsonDoor === "y") {
-    console.log("\nYou knocked but no one answered.");
-    console.log(
-      "\nThere is a strong blast of wind and thunder. It appears that a storm is on the way."
-    );
-    console.log("\nYou are scare, so you are attempting to open the door.");
-    // undefined issue
-    console.log(
-      "\nAnd because the door was not secured, you chose to enter, expecting that the storm would pass quickly."
-    );
-  } else {
-    console.log("\nYou choose to ignore it by taking a nap.");
-
-    console.log("\nYou went home after taking a dump at your favourite spot.");
-
-    console.log(
-      "\nYou take another nap, hoping your mother will return to feed you."
-    );
+  console.log("\n----------------\nThe commands are (y)es or (n)o");
+  crimsonDoor = rl.question("\nDo you want to knock? (y/n) ");
+  while (crimsonDoor === "y") {
+    if (crimsonDoor === "y") {
+      console.log(
+        "\nYou knocked but no one answered.\n\nThere is a strong blast of wind and thunder. It appears that a storm is on the way.\n\nYou are scare, so you are attempting to open the door\n\nAnd because the door was not secured, you chose to enter, expecting that the storm would pass quickly."
+      );
+      console.log(
+        "\nWhen you went inside, you shut the door and heard joyous music, as if there was a party farther down the tunnel.\n\nAnd you decided to follow the music to discover where it came from.\n\nYou noticed after 5 minutes of walking that you had completely forgotten you were sick from the mushroom, but there is a dividing point at the tunnel."
+      );
+    } else if (crimsonDoor === "n") {
+      console.log(
+        "\nYou choose to ignore it by taking a nap.\n\nYou went home after taking a dump at your favourite spot.\n\nYou take another nap, hoping your mother will return to feed you."
+      );
+    } else {
+      console.log("Come on!!! Is it that hard to enter (y)es or (n)o?");
+      crimsonDoor = rl.question("Are you going to knock on the door?! (y/n) ");
+    }
   }
 }
 
+function caveEntry(goRight) {
+  console.log("\n----------------\nThe commands are (r)ight or (l)eft");
+  crimsonDoor = rl.question(
+    "\nShould you go right and follow the music or go left? (r/l) "
+  );
+  while (goRight != "r" && goRight != "l") {
+    if (goRight === "r") {
+      break;
+    } else if (goRight === "l") {
+      console.log(
+        "\nThis path is tedious since there is no music and it is dark. You came to a dead end.\n\nHold on... you see something on the wall. Someone engraved “A89C90” upside down. What does that mean?\n\nIt's most likely nothing, so you turn around and decide to pursue the correct path."
+      );
+      break;
+    } else {
+      console.log(
+        "Come on!!! Do you know where you are going? Pick (r)ight or (l)eft"
+      );
+      goRight = rl.question("Make up your mind! (r/l) ");
+    }
+  }
+  console.log("\nYou chose to take the path where the music is playing.");
+}
 // console.log(gameStart("y"));
 // console.log(enterForest("y"));
 // console.log(massiveTree("y"));
 
 // This needs to be fix
-function chubAdven(playGame, eatMush, crimsonDoor) {
+function chubAdven(playGame, eatMush, crimsonDoor, goRight) {
   while (true) {
     console.log("Welcome");
     gameStart(playGame);
     lookMom();
     enterForest(eatMush);
     massiveTree(crimsonDoor);
+    caveEntry(goRight);
     console.log("The end");
     break;
   }
