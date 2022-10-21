945738353b0684178661e307cb1fdb02d4ff31ae
George <gwmkwan@gmail.com>
2022-10-20 22:00:53 -0600

    added gameOne function

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ import {
   crimsonNo,
   goRightYes,
   goRightNo,
+  winCoinFlip,
+  shinyCoinCollect,
+  tinyCreatureLeft,
 } from "./story.js";
 
 const app = express();
@@ -24,6 +27,7 @@ app.use(express.json()); // send array or object to the backend
 //   testingMessage();
 //   res.send(testingMessage());
 // });
+let items = [];
 
 app.get("/gameStart", (req, res) => {
   const message = gameStartStory;
@@ -62,7 +66,7 @@ app.get("/crimsonDoor", (req, res) => {
   res.send(message);
 });
 
-app.get("/goRight", (req, res) => {
+app.get("/caveEntry", (req, res) => {
   const answer = req.query.answer;
   let message;
   if (answer === "r") {
@@ -73,26 +77,46 @@ app.get("/goRight", (req, res) => {
   res.send(message);
 });
 
-export let playerGuess = playerInput === "h" ? "heads" : "tails";
-export let compGuess = Math.random() * 1 > 0.5 ? "heads" : "tails";
-
-app.get("/playGuess", (req, res) => {
-  const answer = req.query.answer;
-  let message;
-
-  // if (answer === "r") {
-  //   message = goRightYes;
-  // } else {
-  //   message = goRightNo;
-  // }
+app.get("/gameOneInstruc", (req, res) => {
+  const message = gameOneInstru;
   res.send(message);
 });
 
-// app.post("/projectOne", (req, res) => {
-//   console.log("Item added");
-//   const item = request.body;
-//   items.push(item);
-//   res.send();
-// });
+app.get("/gameOne", (req, res) => {
+  const playerInput = req.query.answer;
+  let message;
+  // console.log("\n----------------\nThe commands are (h)eads or (t)ails");
+
+  // let playerInput = rl.keyIn("\nDo you pick heads or tails? (h/t) ", {
+  //   hideEchoBack: false,
+  //   mask: "",
+  //   limit: "ht",
+  // });
+
+  let playerGuess = playerInput === "h" ? "heads" : "tails";
+  let compGuess = Math.random() * 1 > 0.5 ? "heads" : "tails";
+
+  while (compGuess !== playerGuess) {
+    message = `\n***The coin result is ${compGuess} but you guessed ${playerGuess}, try again :(***`;
+    // console.log("\n----------------\nThe commands are (h)eads or (t)ails");
+
+    // playGuess = rl.keyIn("\nDo you pick heads or tails? (h/t) ", {
+    //   hideEchoBack: false,
+    //   mask: "",
+    //   limit: "ht",
+    // });
+    playerGuess = playerInput === "h" ? "heads" : "tails";
+
+    compGuess = Math.random() * 1 > 0.5 ? "heads" : "tails";
+  }
+  message =
+    `***The coin result is ${compGuess} and you guessed ${playerGuess}, you win :)***` +
+    winCoinFlip +
+    shinyCoinCollect +
+    tinyCreatureLeft;
+  items.push("shiny coin");
+
+  res.send(message);
+});
 
 app.listen(port, () => console.log(`My game server running on port ${port}!`));
--- story.js
+++ story.js
@@ -1,25 +1,41 @@
 export function testingMessage() {
-  return "\n--------Testing--------\n";
-}
-export function yesNoCommands() {
-  return "\n----------------\nThe commands are (y)es or (n)o";
-}
-export function yesCommands() {
-  return "\n----------------\nPlease enter (y)es when you are ready.";
-}
-export function gameStartStory() {
-  return "\nYou are a bichon shih tzu. You enjoy sleeping and eating on a daily basis. Your mother always says you're fearless and enjoy adventure, but the only places you've been are around the neighbourhood and the off-leash park. But one day everything changes...";
-}
-export function wokeUpHome() {
-  return "\n***You awoke at home, but your mother was not to be seen, and you are concerned for her since you are hungry.\n\nSo you decided to walk outdoors and look around for your mom.\n\nBecause your mom has gone missing, you've chosen to walk into the wilderness and look for her.";
-}
-export function niceMushroom() {
-  return "\nYou spotted a nice-looking mushroom not long after entering the wilderness, and you're starving.";
+  return "\n--------***Testing***story.js--------\n";
 }
 
-export function eatMushYes() {
-  return "\nYou weren't feeling well, so you opted to relax behind a massive tree.\n\nYou see something on the other side of the tree as you get closer to it.\n\nSo you went to have a peek and discovered a crimson door.";
-}
-export function eatMushNo() {
-  return "\nTaking a look around.\n\nYou've decided to stay at home because the outside is too frightening for you; you'll try again tomorrow.";
-}
+export const gameStartStory =
+  "\nYou are a bichon shih tzu. You enjoy sleeping and eating on a daily basis. Your mother always says you're fearless and enjoy adventure, but the only places you've been are around the neighbourhood and the off-leash park. But one day everything changes...";
+
+export const wokeUpHome =
+  "\n***You awoke at home, but your mother was not to be seen, and you are concerned for her since you are hungry.\n\nSo you decided to walk outdoors and look around for your mom.\n\nBecause your mom has gone missing, you've chosen to walk into the wilderness and look for her.\n";
+
+export const niceMushroom =
+  "\nYou spotted a nice-looking mushroom not long after entering the wilderness, and you're starving.";
+
+export const eatMushYes =
+  "\nYou weren't feeling well, so you opted to relax behind a massive tree.\n\nYou see something on the other side of the tree as you get closer to it.\n\nSo you went to have a peek and discovered a crimson door.";
+
+export const eatMushNo =
+  "\nTaking a look around.\n\nYou've decided to stay at home because the outside is too frightening for you; you'll try again tomorrow.";
+
+export const crimsonYes =
+  "\nYou knocked but no one answered.\n\nThere is a strong blast of wind and thunder. It appears that a storm is on the way.\n\nYou are scare, so you are attempting to open the door\n\nAnd because the door was not secured, you chose to enter, expecting that the storm would pass quickly.\n\nWhen you went inside, you shut the door and heard joyous music, as if there was a party farther down the tunnel.\n\nAnd you decided to follow the music to discover where it came from.\n\nYou noticed after 5 minutes of walking that you had completely forgotten you were sick from the mushroom, but there is a dividing point at the tunnel.";
+
+export const crimsonNo =
+  "\nYou choose to ignore it by taking a nap.\n\nYou went home after taking a dump at your favourite spot.\n\nYou take another nap, hoping your mother will return to feed you.";
+
+export const goRightYes =
+  "\nYou chose to take the path where the music is playing.";
+
+export const goRightNo = `\nThis path is tedious since there is no music and it is dark. You came to a dead end.\n\nHold on... you see something on the wall. Someone engraved “A89C90” upside down. What does that mean?\n\nIt's most likely nothing, so you turn around and decide to pursue the correct path.\nHold on...\nyou see something on the wall.\nSomeone engraved “A89C90” upside down. What does that mean?\nIt's most likely nothing, so you turn around and decide to pursue the correct path.\nYou proceeded down the tunnel. It's been a while, and you're curious, so you decided to look back. The path appears to be the same as continuing on.\nSo you proceed down the tube until a little creature that resembles a person arrives.
+\nTiny creature: You know you don't belong here...
+\nChubby: Where am I? I am lost
+\nTiny creature: I might tell you but first you have to play a game with me, if you win, I will tell you.\nYou agreed to play the game out of desperation.`;
+
+export const winCoinFlip = `\n\nTiny creature: That was great; I don't usually get visitors, but since you won, I'll honour my promise. Raizel, Daughter of the Soul Eater, owns this world and enjoys collecting young pups. Many puppies end up down here and never make it back to their owners.
+\n(foot step noise)
+\nTiny creature(whisper): I shouldn't say too much about her; Here, keep this coin you will need it to get out.\nBest of luck little guy!\n`;
+
+export const shinyCoinCollect = `\n***You have collected a shiny coin***\n`;
+
+export const tinyCreatureLeft =
+  "\nThe tiny creature vanished with a poof! And you keep going down into the gloomy tunnel.\n";
