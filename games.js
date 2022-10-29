d2f066e0177bda0ae6a77389952955bdcdf12102
George <gwmkwan@gmail.com>
2022-10-28 22:33:06 -0600

    minor game optimization

--- games.js
+++ games.js
@@ -1,46 +1,6 @@
 'use strict';
 
 import chalk from 'chalk';
-// export let diceRollMessage, magicMessage;
-
-// let playerInput = 'h';
-// export let playerGuess = playerInput === 'h' ? 'heads' : 'tails';
-// export let compGuess = Math.random() > 0.5 ? 'heads' : 'tails';
-
-// export function coinFlip(playerGuess, compGuess) {
-//   console.log(chalk.blue.bold(`You guess: ${playerGuess}`));
-//   console.log(chalk.red.bold(`The coin flip result is: ${compGuess}`));
-//   if (playerGuess !== compGuess) {
-//     return 0;
-//   } else {
-//     return 1;
-//   }
-// }
-// coinFlip(playerGuess, compGuess);
-// let coinFlipMessageLoss, coinFlipMessageWin;
-// export function coinFlipMessage(compGuess, playerGuess) {
-//   if (coinFlip === 0) {
-//     coinFlipMessageLoss = `***The coin result is ${compGuess} but you guessed ${playerGuess}, try again :(***`;
-//     console.log(chalk.red.bold(coinFlipMessageLoss));
-//     return coinFlipMessageLoss;
-//   } else {
-//     coinFlipMessageWin =
-//       `***The coin result is ${compGuess} and you guessed ${playerGuess}, you win :)***` +
-//       console.log(chalk.green.bold(coinFlipMessageWin));
-//     return coinFlipMessageWin;
-//   }
-// }
-
-// export function diceRoll() {
-//   let playerRoll = Math.floor(Math.random() * 40 + 1);
-//   let compRoll = Math.floor(Math.random() * 40 + 1);
-//   console.log('Player ' + playerRoll + ' vs.' + 'Grumpy ' + compRoll);
-
-//   return {
-//     playerRoll: playerRoll,
-//     compRoll: compRoll,
-//   };
-// }
 
 export function fiveElements(gamerInput) {
   function getUserChoice() {
