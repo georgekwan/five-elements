8e9e15b331b4484e89facfb29b52227466347d1b
George <gwmkwan@gmail.com>
2023-07-17 14:03:45 -0600

    fix: add check player input

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ app.use(express.json()); // send array or object to the backend
 app.get('/gameThree', (req, res) => {
   const gamerInput = req.query.playerInput;
   console.log(gamerInput);
-  const results = fiveElements(gamerInput);
-  console.log(results);
-  res.send(results);
-  // add setDelay
+
+  if (gamerInput) {
+    const results = fiveElements(gamerInput);
+    console.log('🪵 ----------------------------------------------------🪵');
+    console.log('🪵 ~ file: server.js:18 ~ app.get ~ results:', results);
+    console.log('🪵 ----------------------------------------------------🪵');
+    res.send(results);
+  } else {
+    res.status(400).send('Missing playerInput parameter');
+  }
 });
 
 app.listen(port, () => console.log(`My game server running on port ${port}!`));
